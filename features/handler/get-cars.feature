a77ddba6a0f1fe83b7d534c4967cf3b007ef8278
Edson Cabral Junior <e2cabraljunior@gmail.com>
2022-12-21 15:42:10 -0300

    🧪 (cucumber) add a falling test

--- features/handler/get-cars.feature
+++ features/handler/get-cars.feature
@@ -4,3 +4,7 @@ Feature: Get Cars
     Given A car model and make
     When The model is a4 and the make is audi
     Then The data returned should be a list of audi a4
+
+  Scenario: Return an error if no parameters were provided
+    When No car model and make were provided
+    Then Should return a http server error message
--- features/step-definitions/get-cars.steps.ts
+++ features/step-definitions/get-cars.steps.ts
@@ -6,6 +6,7 @@ import {
     APIGatewayProxyEvent,
     APIGatewayProxyResult
 } from "aws-lambda";
+import {serverError} from "../../src/helpers/http.helper";
 
 const mockRequest = (model: string | null, make: string | null) => ({
     body: null,
@@ -22,20 +23,40 @@ const mockRequest = (model: string | null, make: string | null) => ({
     resource: 'create',
 } as APIGatewayProxyEvent);
 
-let expected: APIGatewayProxyResult;
+let actual: APIGatewayProxyResult;
 let model: string;
 let make: string;
 
+//# region First Step
 Given(/^A car model and make$/, async () => {
     model = 'a4'
     make = 'audi'
 });
 
 When(/^The model is a4 and the make is audi$/, async () => {
-    expected = await handle(mockRequest(model, make))
+    actual = await handle(mockRequest(model, make))
 });
 
 Then(/^The data returned should be a list of audi a4$/, async () => {
     const listToCompareTypes: any[] = [];
-    assert.equal(typeof listToCompareTypes, typeof expected)
+    assert.equal(
+        typeof listToCompareTypes,
+        typeof actual.body,
+        'The expected parameter is not equal to the default'
+    );
 });
+//# endregion
+
+//# region Second Step
+When(/^No car model and make were provided$/, async () => {
+    actual = await handle(mockRequest(null, null));
+});
+
+Then(/^Should return a http server error message$/, () => {
+    const expected = serverError(new Error('You must provide at least a make.'));
+    const actualToCompare = JSON.stringify(actual.body);
+
+    assert.equal(actual.statusCode, expected.statusCode);
+    assert.equal(JSON.parse(actualToCompare).name, expected.body.name);
+})
+//# endregion
