12fad79cf7bbcab99034d8ecaab125a97ec0bee8
Edson Cabral Junior <e2cabraljunior@gmail.com>
2022-12-21 15:41:04 -0300

    🚧 add validation for the make and model parameters

--- src/handler/app.ts
+++ src/handler/app.ts
@@ -1,8 +1,16 @@
 import {APIGatewayEvent, APIGatewayProxyResult} from "aws-lambda";
 import {ok, serverError} from "../helpers/http.helper";
 
+type QueryStringGetCarsParam = { model: string, make: string }
+
 export const handle = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
+    const { model, make } = event.queryStringParameters as QueryStringGetCarsParam;
+
     try {
+        if ((!model && !make) || !make) {
+            throw new Error('You must provide at least a make.');
+        }
+
         return ok([{ model: 'a4', make: 'audi' }])
     } catch (err) {
         return serverError((err as Error))
